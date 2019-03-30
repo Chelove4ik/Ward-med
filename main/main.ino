5197344766931e177ec9f5c60f98568ff34b20e2
RashX2001 <49086922+RashX2001@users.noreply.github.com>
2019-03-30 16:37:58 +1000

    pulse

---
+++ main/main.ino
@@ -0,0 +1,25 @@
+#include <TimerOne.h>                                                                                                  
+int val = 0;
+int i=0;
+T1=0;
+T2=0;                                
+void sendData() 
+{                                                                                        
+  Serial.write("A0");                                                                                                          
+  val = analogRead(A0);                                                              
+  Serial.write(map(val, 0, 1023, 0, 255));
+  if (val>480) 
+  {
+    i++;
+    
+  }
+                                            
+}
+void setup() {            
+  Serial.begin(9600);                     
+  Timer1.initialize(3000);                  
+  Timer1.attachInterrupt(sendData);         
+}
+void loop() 
+{
+}
