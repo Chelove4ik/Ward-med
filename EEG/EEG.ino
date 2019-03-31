b7d6ab74bb3fbe7fa47cf4ddfb32dd94bc3c647b
Anton <mr.kur.anton@gmail.com>
2019-03-31 17:12:12 +1000

    create EEG

---
+++ EEG/EEG.ino
@@ -0,0 +1,18 @@
+#include <TimerOne.h>
+
+int val = 0;
+int speed = 9600;
+          
+void sendData(){                                              
+  val = analogRead(A1);                     
+  Serial.println(val);
+}
+
+void setup() {
+  Serial.begin(speed);                     
+  Timer1.initialize(3000);                  
+  Timer1.attachInterrupt(sendData);     
+}
+
+void loop() {
+}
