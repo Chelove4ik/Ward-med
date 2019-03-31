4311701f3be4b846e62585f26ff2cc53709fda22
RashX2001 <49086922+RashX2001@users.noreply.github.com>
2019-03-31 15:06:54 +1000

    Update main.ino
    1245

--- main/main.ino
+++ main/main.ino
@@ -1,19 +1,33 @@
 #include <TimerOne.h>                                                                                                  
 int val = 0;
 int i=0;
-T1=0;
-T2=0;                                
+int pulse=0;
+int sumdelT=0;
+int T1=0;
+int T2=0;                                
 void sendData() 
 {                                                                                        
-  Serial.write("A0");                                                                                                          
+  //Serial.write("A0");                                                                                                          
   val = analogRead(A0);                                                              
-  Serial.write(map(val, 0, 1023, 0, 255));
+ // Serial.write(map(val, 0, 1023, 0, 255));
   if (val>480) 
   {
     i++;
-    
-  }
-                                            
+    if (i%2==0) 
+    {
+    T2=millis();
+    sumdelT+=T2-T1;
+    } else if (i%2>0)
+    {
+     T1=millis();
+    }
+    if (i>=10) 
+    {
+      pulse=1.0/sumdelT*100.0;    
+      Serial.println(pulse); 
+      sumdelT=0;  
+    }
+  }                                            
 }
 void setup() {            
   Serial.begin(9600);                     
