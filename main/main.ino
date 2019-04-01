4eb43313eb42d68cfa8d79ae5438971271381bc2
Anton <mr.kur.anton@gmail.com>
2019-04-01 16:42:03 +1000

    send random BPM and GSR to local server

--- main/main.ino
+++ main/main.ino
@@ -1,41 +1,21 @@
-#include <TimerOne.h>                                                                                                  
-int val = 0;
-int i=0;
-int pulse=0;
-int sumdelT=0;
-int T1=0;
-int T2=0;                                
-void sendData() 
-{                                                                                        
-  //Serial.write("A0");                                                                                                          
-  val = analogRead(A0);                                                              
- // Serial.write(map(val, 0, 1023, 0, 255));
-  if (val>480) 
-  {
-    i++;
-    if (i%2==0) 
-    {
-    T2=millis();
-    sumdelT+=T2-T1;
-    } else if (i%2>0)
-    {
-     T1=millis();
-    }
-    if (i>=10) 
-    {
-      pulse=1.0/sumdelT*100.0;    
-      Serial.println(pulse); 
-      sumdelT=0;  
-    }
-  }                                            
+#include <TimerOne.h> 
+
+int BPM = 0, GSR = 0;
+int speed = 9600;
+
+void sendData(){    
+  Serial.print("BPM ");
+  Serial.println(random(60, 100));                                          
+  GSR = analogRead(A2); 
+  Serial.print("GSR ");                    
+  Serial.println(GSR);
 }
-void setup() {            
-  Serial.begin(9600);                     
+
+void setup() {
+  Serial.begin(speed);                     
   Timer1.initialize(3000);                  
-  Timer1.attachInterrupt(sendData);         
+  Timer1.attachInterrupt(sendData);     
 }
 
-
-void loop() 
-{
-}
+void loop() {
+} 
