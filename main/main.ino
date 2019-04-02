3812ff6f6f72067e45f74e658989a819f9ffb272
Anton <mr.kur.anton@gmail.com>
2019-04-02 17:52:37 +1000

    add sendPulse and sendGSR functions

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,57 @@
 #include <TimerOne.h> 
 
-int BPM = 0, GSR = 0;
+int GSR = 0;
+int BPM = 0, oldpulse = 0, pulses = 0, T = 0, Td = 0, BPMold = 0,pulse_per_sec = 0, sum_pulse = 0, i = 0;
+bool flag = false;
+char GSRport[2] = "A2", BPMport[2] = "A0";
+
 int speed = 9600;
 
-void sendData(){    
-  Serial.print("BPM ");
-  Serial.println(random(60, 100));                                          
-  GSR = analogRead(A2); 
+void sendPulse(){    
+  BPM = map(analogRead(BPMport), 0, 1023, 0, 256);
+  if (BPM > BPMold && !flag)
+  {
+    flag = true;
+  }
+
+  if (BPM < BPMold && flag)
+  {
+    flag = false;
+    pulses ++;
+    if (pulses == 1)
+    {
+      T = millis();
+      delay(3000);
+    }
+    if (pulses == 2)
+    {
+      Td = millis() - T;
+      pulses = 0;
+    }
+    pulse_per_sec = 1000.0 / Td * 60;
+    if (i < 7)
+    {
+      sum_pulse += pulse_per_sec;
+      i++;
+    }
+    else
+    {
+        if ((abs((sum_pulse / 7.0) - oldpulse) < 25) && (sum_pulse / 7.0 < 150) && (sum_pulse / 7.0 > 0))
+      {
+            Serial.print("BPM ");
+            Serial.println(int(sum_pulse / 7.0));
+      }
+      oldpulse = sum_pulse / 7.0;
+      sum_pulse = 0;
+      i = 0;
+    }
+  }
+  BPMold = BPM;
+}
+
+void sendGSR() {
+  delay(3000);               
+  GSR = analogRead(GSRport); 
   Serial.print("GSR ");                    
   Serial.println(GSR);
 }
@@ -14,8 +59,9 @@ void sendData(){
 void setup() {
   Serial.begin(speed);                     
   Timer1.initialize(3000);                  
-  Timer1.attachInterrupt(sendData);     
+  Timer1.attachInterrupt(sendPulse);     
 }
 
 void loop() {
+  sendGSR();
 } 
